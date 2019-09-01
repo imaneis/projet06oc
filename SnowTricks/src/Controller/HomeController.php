bbf3fa4b0a36dfd648598ee8ea280f60a145b42e
imaneis <issanyimane@perso.be>
2019-09-01 23:28:45 +0200

    add comment section

--- SnowTricks/src/Controller/HomeController.php
+++ SnowTricks/src/Controller/HomeController.php
@@ -5,43 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use  App\Repository\ArticleRepository;
-use App\Entity\Comment;
-use App\Form\CommentType;
-use Symfony\Component\HttpFoundation\Request;
-use Doctrine\Common\Persistence\ObjectManager;
-use App\Entity\User;
-use App\Entity\Article;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(ArticleRepository $repo, Request $request, ObjectManager $manager)
+    public function index(ArticleRepository $repo)
     {
-        $comment = new Comment();
-        $form = $this->createForm(CommentType::class, $comment);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            
-            $article = $repo->find();
-
-            $comment->setCreatedAt(new \DateTime())
-                    ->setArticle($article);
-
-            $manager->persist($comment);
-            $manager->flush();
-
-            return $this->redirectToRoute('home');
-        }
 
     	$articles = $repo->findAll();
     	
         return $this->render('home/index.html.twig', [
-        	'articles' => $articles,
-            'commentForm' => $form->createView()
+        	'articles' => $articles
         ]);
     }
 }
---
+++ SnowTricks/src/Controller/TricksController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Comment;
+use App\Form\CommentType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\User;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+
+class TricksController extends AbstractController
+{
+    /**
+     * @Route("/tricks/{id}", name="tricks")
+     */
+    public function index(Article $article, Request $request, ObjectManager $manager)
+    {
+
+    	$comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+         $username = $this->getUser()->getUsername();
+
+            $comment->setCreatedAt(new \DateTime())
+                    ->setArticle($article)
+                    ->setAuthor($username);
+            $manager->persist($comment);
+            $manager->flush();
+
+            return $this->redirectToRoute('tricks', ['id' => $article->getId()]);
+        }
+
+
+        return $this->render('tricks/index.html.twig', [
+            'trick' => $article,
+            'commentForm' => $form->createView()
+        ]);
+    }
+}
