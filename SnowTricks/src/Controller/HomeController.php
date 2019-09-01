29b46610e0cf33cfd2634bff581f231a868437cf
imaneis <issanyimane@perso.be>
2019-09-01 22:50:46 +0200

    fix issues of posting articles

--- SnowTricks/src/Controller/HomeController.php
+++ SnowTricks/src/Controller/HomeController.php
@@ -5,21 +5,43 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use  App\Repository\ArticleRepository;
+use App\Entity\Comment;
+use App\Form\CommentType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\User;
+use App\Entity\Article;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(ArticleRepository $repo)
+    public function index(ArticleRepository $repo, Request $request, ObjectManager $manager)
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $article = $repo->find();
+
+            $comment->setCreatedAt(new \DateTime())
+                    ->setArticle($article);
+
+            $manager->persist($comment);
+            $manager->flush();
+
+            return $this->redirectToRoute('home');
+        }
 
     	$articles = $repo->findAll();
-
-        
     	
         return $this->render('home/index.html.twig', [
-        	'articles' => $articles
+        	'articles' => $articles,
+            'commentForm' => $form->createView()
         ]);
     }
 }
